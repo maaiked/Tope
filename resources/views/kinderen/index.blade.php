775690c539c18af09cd12df5bfc477281afc605e
Maaiked <maaike.dupont@student.ehb.be>
2024-03-26 14:38:36 +0100

    updated attributen kind

--- resources/views/kinderen/index.blade.php
+++ resources/views/kinderen/index.blade.php
@@ -2,7 +2,7 @@
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
        @if (!auth()->user()->isAdmin) <form method="POST" action="{{ route('kinderen.store') }}">
             @csrf
-            <textarea>
+            <textarea
                 name="name"
                 placeholder="{{ __('Geef de naam van je kind op') }}"
                 class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
