6071bfd0ca9e50f70f16a966594f4908596b332e
Maaiked <maaike.dupont@student.ehb.be>
2024-03-28 15:27:02 +0100

    minor changes

--- resources/views/kinderen/indexOuder.blade.php
+++ resources/views/kinderen/indexOuder.blade.php
@@ -35,7 +35,7 @@
 
             </div>
             <button onclick="window.location='{{ route("kind.create") }}'"
-                    button class="bg-blue-500 hover:bg-blue-700 mt-2 text-white font-bold py-2 px-4 rounded">
+                    class="bg-blue-500 hover:bg-blue-700 mt-2 text-white font-bold py-2 px-4 rounded">
                 Kind toevoegen
             </button>
         </div>
