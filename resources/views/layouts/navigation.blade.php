1830ffc49863a40324170566d93c674256eb8d97
Maaiked <maaike.dupont@student.ehb.be>
2024-04-13 19:12:42 +0200

    minor fix for dropdown not working on production

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: true }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -29,6 +29,10 @@
                         {{ __('Profiel') }}
                         </x-nav-link>
                     @endif
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    </form>
 
                 </div>
             </div>
