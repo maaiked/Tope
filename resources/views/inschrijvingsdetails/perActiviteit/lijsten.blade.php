da5bffe0a01f6542cad1d8c70c46ee100a055d32
Maaiked <maaike.dupont@student.ehb.be>
2024-04-18 18:32:23 +0200

    updated animatorview: added participants lists per activity : medical, options, allergie, medicatie,

--- resources/views/inschrijvingsdetails/perActiviteit/lijsten.blade.php
+++ resources/views/inschrijvingsdetails/perActiviteit/lijsten.blade.php
@@ -17,13 +17,16 @@
             {{--                TODO:: fix small screen colums--}}
 
             <button class=" rounded-md bg-blue-500 text-white focus:ring-gray-600 px-2 py-2 text-sm"
-                    onclick="window.history.back()">Terug naar activiteiten</button>
+                    onclick="window.history.back()">Terug naar activiteiten
+            </button>
 
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg px-6 py-2 gap-4 gap-y-2 text-sm mt-2">
                 {{-- Lijst ingeschreven kinderen --}}
                 <div>
-                    <h3 class="font-semibold text-lg text-gray-800 leading-tight">Ingeschreven kinderen: toon alle ingeschreven kinderen</h3>
-                    <p class="text-md text-gray-800 leading-tight">voornaam - familienaam - leerjaar - ouder - afhalen - alleennaarhuis - foto</p>
+                    <h3 class="font-semibold text-lg text-gray-800 leading-tight">Ingeschreven kinderen: toon alle
+                        ingeschreven kinderen</h3>
+                    <p class="text-md text-gray-800 leading-tight">voornaam - familienaam - leerjaar - ouder - afhalen -
+                        alleennaarhuis - foto</p>
                     <button
                         class="rounded-md bg-gray-500 text-white focus:ring-gray-600 px-2 py-2 text-sm"
                         onclick="window.location='{{ route("inschrijvingsdetails.showLijst", [$activiteit->id, "alleKinderen"]) }}'">
@@ -35,8 +38,10 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg px-6 py-2 gap-4 gap-y-2 text-sm mt-2">
                 {{-- Lijst contactgegevens --}}
                 <div>
-                    <h3 class="font-semibold text-lg text-gray-800 leading-tight">Lijst ingeschreven kinderen met contactgegevens</h3>
-                    <p class="text-md text-gray-800 leading-tight">voornaam - familienaam - leerjaar - ouder: telefoonnummer - extra contactpersoon</p>
+                    <h3 class="font-semibold text-lg text-gray-800 leading-tight">Lijst ingeschreven kinderen met
+                        contactgegevens</h3>
+                    <p class="text-md text-gray-800 leading-tight">voornaam - familienaam - leerjaar - ouder:
+                        telefoonnummer - extra contactpersoon</p>
                     <button
                         class="rounded-md bg-gray-500 text-white focus:ring-gray-600 px-2 py-2 text-sm"
                         onclick="window.location='{{ route("inschrijvingsdetails.showLijst", [$activiteit->id, "alleContact"]) }}'">
@@ -48,8 +53,10 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg px-6 py-2 gap-4 gap-y-2 text-sm mt-4">
                 {{-- Lijst aanwezige kinderen --}}
                 <div>
-                    <h3 class="font-semibold text-lg text-gray-800 leading-tight">Aanwezige kinderen: toon enkel ingecheckte kinderen die nog niet uitgechecked zijn</h3>
-                    <p class="text-md text-gray-800 leading-tight">voornaam - familienaam - leerjaar - ouder - afhalen - alleennaarhuis - foto</p>
+                    <h3 class="font-semibold text-lg text-gray-800 leading-tight">Aanwezige kinderen: toon enkel
+                        ingecheckte kinderen die nog niet uitgechecked zijn</h3>
+                    <p class="text-md text-gray-800 leading-tight">voornaam - familienaam - leerjaar - ouder - afhalen -
+                        alleennaarhuis - foto</p>
                     <button
                         class="rounded-md bg-gray-500 text-white focus:ring-gray-600 px-2 py-2 text-sm"
                         onclick="window.location='{{ route("inschrijvingsdetails.showLijst", [$activiteit->id, "aanwezigeKinderen"]) }}'">
@@ -62,7 +69,8 @@
                 {{-- Lijst ingeschreven opties --}}
                 <div>
                     <h3 class="font-semibold text-lg text-gray-800 leading-tight">Inschrijvingen per optie</h3>
-                    <p class="text-md text-gray-800 leading-tight">voornaam - familienaam - leerjaar - optie - afhalen - alleennaarhuis - foto</p>
+                    <p class="text-md text-gray-800 leading-tight">voornaam - familienaam - leerjaar - optie - afhalen -
+                        alleennaarhuis - foto</p>
                     <button
                         class="rounded-md bg-gray-500 text-white focus:ring-gray-600 px-2 py-2 text-sm"
                         onclick="window.location='{{ route("inschrijvingsdetails.showLijst", [$activiteit->id, "opties"]) }}'">
@@ -75,7 +83,8 @@
                 {{-- Lijsten medische gegevens --}}
                 <div>
                     <h3 class="font-semibold text-lg text-gray-800 leading-tight">Medische info: alles</h3>
-                    <p class="text-md text-gray-800 leading-tight">voornaam - familienaam - leerjaar - ouder - telefoonnummer ouder - contactpersoon - allergie - beperking - medicatie</p>
+                    <p class="text-md text-gray-800 leading-tight">voornaam - familienaam - leerjaar - ouder -
+                        telefoonnummer ouder - contactpersoon - allergie - beperking - medicatie</p>
                     <button
                         class="rounded-md bg-gray-500 text-white focus:ring-gray-600 px-2 py-2 text-sm"
                         onclick="window.location='{{ route("inschrijvingsdetails.showLijst", [$activiteit->id, "medisch"]) }}'">
@@ -84,33 +93,33 @@
                 </div>
             </div>
 
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg px-6 py-2 gap-4 gap-y-2 text-sm mt-4">
-                    {{-- Lijsten allergie --}}
-                    <div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg px-6 py-2 gap-4 gap-y-2 text-sm mt-4">
+                {{-- Lijsten allergie --}}
+                <div>
                     <h3 class="font-semibold text-lg text-gray-800 leading-tight">Allergieën</h3>
-                    <p class="text-md text-gray-800 leading-tight">voornaam - familienaam - leerjaar - ouder - telefoonnummer ouder - contactpersoon - allergie</p>
-                        <button
-                            class="rounded-md bg-gray-500 text-white focus:ring-gray-600 px-2 py-2 text-sm"
-                            onclick="window.location='{{ route("inschrijvingsdetails.showLijst", [$activiteit->id, "allergie"]) }}'">
-                            {{ "bekijken" }}
-                        </button>
+                    <p class="text-md text-gray-800 leading-tight">voornaam - familienaam - leerjaar - ouder -
+                        telefoonnummer ouder - contactpersoon - allergie</p>
+                    <button
+                        class="rounded-md bg-gray-500 text-white focus:ring-gray-600 px-2 py-2 text-sm"
+                        onclick="window.location='{{ route("inschrijvingsdetails.showLijst", [$activiteit->id, "allergie"]) }}'">
+                        {{ "bekijken" }}
+                    </button>
                 </div>
 
-                </div>
-                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg px-6 py-2 gap-4 gap-y-2 text-sm mt-4">
-                        {{-- Lijsten medicatie --}}
-                        <div>
-                    <h3 class="font-semibold text-lg text-gray-800 leading-tight">Medicatie</h3>
-                    <p class="text-md text-gray-800 leading-tight">voornaam - familienaam - leerjaar - ouder - telefoonnummer ouder - contactpersoon - medicatie</p>
-                            <button
-                                class="rounded-md bg-gray-500 text-white focus:ring-gray-600 px-2 py-2 text-sm"
-                                onclick="window.location='{{ route("inschrijvingsdetails.showLijst", [$activiteit->id, "medicatie"]) }}'">
-                                {{ "bekijken" }}
-                            </button>
-                </div>
-                    </div>
             </div>
-
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg px-6 py-2 gap-4 gap-y-2 text-sm mt-4">
+                {{-- Lijsten medicatie --}}
+                <div>
+                    <h3 class="font-semibold text-lg text-gray-800 leading-tight">Medicatie</h3>
+                    <p class="text-md text-gray-800 leading-tight">voornaam - familienaam - leerjaar - ouder -
+                        telefoonnummer ouder - contactpersoon - medicatie</p>
+                    <button
+                        class="rounded-md bg-gray-500 text-white focus:ring-gray-600 px-2 py-2 text-sm"
+                        onclick="window.location='{{ route("inschrijvingsdetails.showLijst", [$activiteit->id, "medicatie"]) }}'">
+                        {{ "bekijken" }}
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
 </x-app-layout>
