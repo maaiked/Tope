f1140dfe0ead4d999fa83601e75322d83325cdbb
yonicelen <yoni.celen@student.ehb.be>
2024-04-20 16:20:19 +0200

    Removed Edit option from indexAdmin.blade.php

--- resources/views/kinderen/indexAdmin.blade.php
+++ resources/views/kinderen/indexAdmin.blade.php
@@ -36,28 +36,6 @@
                             <td class="border px-4 py-2">{{$kind->medicatie}}</td>
                             <td class="border px-4 py-2">{{$kind->infoAdminAnimator}}</td>
                             <td class="border px-4 py-2">{{$kind->infoAdmin}}</td>
-                            <td class="border px-4 py-2">
-                                <div class=" flex space-x-0">
-                                    <button onclick="window.location='{{ route("kind.edit", $kind->id) }}'"
-                                    class="col-span-1  inline-flex border-2 items-center gap-2 rounded-lg px-4 py-2 text-sm text-gray-500 hover:text-gray-900 focus:relative">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke-width="1.5"
-                                        stroke="currentColor"
-                                        class="h-4 w-4"
-                                    >
-                                        <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                                        />
-                                    </svg>
-                                    Bewerk
-                                    </button>
-                                </div>
-                            </td>
                         </tr>
                     @endforeach
 
