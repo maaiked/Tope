f712dedde010af2d6f2f8bd9e82d8e0bb66353a4
yonicelen <yoni.celen@student.ehb.be>
2024-05-13 08:00:06 +0200

    added view indexAdmin(profiel), updated indexAdmin(activiteiten)

--- resources/views/activiteiten/indexAdmin.blade.php
+++ resources/views/activiteiten/indexAdmin.blade.php
@@ -3,7 +3,7 @@
 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Activiteiten van vandaag') }}
+            {{ __('Activiteiten') }}
         </h2>
     </x-slot>
 
---
+++ resources/views/profiel/indexAdmin.blade.php
@@ -0,0 +1,70 @@
+<x-app-layout>
+
+    {{--    made with https://tailwindflex.com/@rp-ketan/datatable --}}
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Alle Ouders') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg px-6 py-6">
+
+                <table id="example" class="bootstrap-table" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
+                    <thead>
+                    <tr>
+                        <th class="px-4 py-2">Voornaam</th>
+                        <th class="px-4 py-2">Familienaam</th>
+                        <th class="px-4 py-2">Straat</th>
+                        <th class="px-4 py-2">Huisnummer</th>
+                        <th class="px-4 py-2">Bus</th>
+                        <th class="px-4 py-2">Postcode</th>
+                        <th class="px-4 py-2">Gemeente</th>
+                        <th class="px-4 py-2">Rijksregisternummer</th>
+                        <th class="px-4 py-2">Telefoonnummer</th>
+                        <th class="px-4 py-2">Kinderen</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($profielen as $profiel)
+                    <tr>
+                        <td class="border px-4 py-2">{{$profiel->voornaam}}</td>
+                        <td class="border px-4 py-2">{{$profiel->familienaam}}</td>
+                        <td class="border px-4 py-2">{{$profiel->straat}}</td>
+                        <td class="border px-4 py-2">{{$profiel->huisnummer}}</td>
+                        <td class="border px-4 py-2">{{$profiel->bus}}</td>
+                        <td class="border px-4 py-2">{{$profiel->postcode}}</td>
+                        <td class="border px-4 py-2">{{$profiel->gemeente}}</td>
+                        <td class="border px-4 py-2">{{$profiel->rijksregisternummer}}</td>
+                        <td class="border px-4 py-2">{{$profiel->telefoonnummer}}</td>
+                        <td class="border px-4 py-2">
+                            @foreach($profiel.kinds as $kind)
+                            {{$kind->voornaam." ".$kind->familienaam}}
+                        </td>
+                    </tr>
+                    @endforeach
+
+                    </tbody>
+                </table>
+
+
+                {{--            scripts voor tabel met search, sorting en paginatie--}}
+
+                <link rel="stylesheet" href="https://cdn.datatables.net/2.0.3/css/dataTables.dataTables.css">
+                <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+                <script src="https://cdn.datatables.net/2.0.3/js/dataTables.js"></script>
+
+
+                <script>
+                    $(document).ready(function() {
+                        $('#example').DataTable({
+                            // Add any customization options here
+                        });
+                    });
+                </script>
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
