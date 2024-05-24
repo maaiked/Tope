4539c86f5c879ec03084f239a06e5358f0d62a5e
Maaiked <maaike.dupont@student.ehb.be>
2024-05-24 21:43:29 +0200

    code clean up

--- app/Http/Controllers/KindController.php
+++ app/Http/Controllers/KindController.php
@@ -16,23 +16,22 @@ class KindController extends Controller
     public function index()
     {
         // als user = admin -> geef alle kinderen weer
-        if (auth()->user()->isAdmin){
+        if (auth()->user()->isAdmin) {
             $kinderen['kinderen'] = Kind::all();
-            return view ('kinderen.indexAdmin')->with($kinderen);
+            return view('kinderen.indexAdmin')->with($kinderen);
         }
+
         // als user = geen admin -> geef enkel eigen kinderen weer
         else {
             $kinderen['kinderen'] = Kind::where('user_id', auth()->id())->get();
-            foreach ($kinderen as $kind)
-            {
+            foreach ($kinderen as $kind) {
                 foreach ($kind as $k) {
-                    if ($k->uitpasDatumCheck < today())
-                    {
+                    if ($k->uitpasDatumCheck < today()) {
                         $this->uitpasInfo($k->id);
                     }
                 }
             }
-            return view ('kinderen.indexOuder')->with($kinderen);
+            return view('kinderen.indexOuder')->with($kinderen);
         }
     }
 
@@ -50,16 +49,16 @@ class KindController extends Controller
     public function store(Request $request)
     {
         $validated = $request->validate([
-            'voornaam'=> 'required|string|max:255',
-            'familienaam'=> 'required|string|max:255',
-            'rijksregisternummer'=> 'required|string|max:40|regex:/^[0-9]{2}[.][0-9]{2}[.][0-9]{2}[-][0-9]{3}[.][0-9]{2}$/',
-            'contactpersoon'=> 'required|string|max:255',
-            'beperking'=> 'nullable|string|max:255',
-            'allergie'=> 'nullable|string|max:255',
-            'medicatie'=> 'nullable|string|max:255',
-            'afhalenKind'=> 'nullable|string|max:255',
-            'infoAdmin'=> 'string|max:510',
-            'infoAdminAnimator'=> 'string|max:510',
+            'voornaam' => 'required|string|max:255',
+            'familienaam' => 'required|string|max:255',
+            'rijksregisternummer' => 'required|string|max:40|regex:/^[0-9]{2}[.][0-9]{2}[.][0-9]{2}[-][0-9]{3}[.][0-9]{2}$/',
+            'contactpersoon' => 'required|string|max:255',
+            'beperking' => 'nullable|string|max:255',
+            'allergie' => 'nullable|string|max:255',
+            'medicatie' => 'nullable|string|max:255',
+            'afhalenKind' => 'nullable|string|max:255',
+            'infoAdmin' => 'string|max:510',
+            'infoAdminAnimator' => 'string|max:510',
             'alleenNaarHuis' => 'boolean',
             'fotoToestemming' => 'boolean',
             'leerjaar' => [Rule::enum(LeerjaarEnum::class)],
@@ -94,11 +93,9 @@ class KindController extends Controller
     {
         // als kindid behoort tot ingelogde user, geef form weer
         $kind = Kind::find($id);
-        if (auth()->id() == $kind->user->id || auth()->user()->isAdmin){
+        if (auth()->id() == $kind->user->id || auth()->user()->isAdmin) {
             return view('kinderen.edit', compact('kind'));
-        }
-
-        //als kindid niet behoort tot de ingelogde user, keer terug naar index
+        } //als kindid niet behoort tot de ingelogde user, keer terug naar index
         else {
             return redirect(route('kinderen.index'));
         }
@@ -111,16 +108,16 @@ class KindController extends Controller
     {
         $kind = Kind::find($id);
         $validated = $request->validate([
-            'voornaam'=> 'required|string|max:255',
-            'familienaam'=> 'required|string|max:255',
-            'rijksregisternummer'=> 'required|string|max:40|regex:/^[0-9]{2}[.][0-9]{2}[.][0-9]{2}[-][0-9]{3}[.][0-9]{2}$/',
-            'contactpersoon'=> 'required|string|max:255',
-            'beperking'=> 'nullable|string|max:255',
-            'allergie'=> 'nullable|string|max:255',
-            'medicatie'=> 'nullable|string|max:255',
-            'afhalenKind'=> 'nullable|string|max:255',
-            'infoAdmin'=> 'string|max:510',
-            'infoAdminAnimator'=> 'string|max:510',
+            'voornaam' => 'required|string|max:255',
+            'familienaam' => 'required|string|max:255',
+            'rijksregisternummer' => 'required|string|max:40|regex:/^[0-9]{2}[.][0-9]{2}[.][0-9]{2}[-][0-9]{3}[.][0-9]{2}$/',
+            'contactpersoon' => 'required|string|max:255',
+            'beperking' => 'nullable|string|max:255',
+            'allergie' => 'nullable|string|max:255',
+            'medicatie' => 'nullable|string|max:255',
+            'afhalenKind' => 'nullable|string|max:255',
+            'infoAdmin' => 'string|max:510',
+            'infoAdminAnimator' => 'string|max:510',
             'alleenNaarHuis' => 'boolean',
             'fotoToestemming' => 'boolean',
             'leerjaar' => [Rule::enum(LeerjaarEnum::class)],
@@ -129,8 +126,7 @@ class KindController extends Controller
         $kind->update($validated);
 
         // als rijksregisternummer wordt aangepast, update uitpas kolommen.
-        if(strcmp($oldrijksregisternummer, $kind->rijksregisternummer ) !== 0)
-        {
+        if (strcmp($oldrijksregisternummer, $kind->rijksregisternummer) !== 0) {
             $this->uitpasInfo($kind->id);
         }
 
@@ -141,43 +137,37 @@ class KindController extends Controller
     {
         $kind = Kind::find($kindid);
 
-            $uitpas = (new UitpasController)->uitpasKind($kind->rijksregisternummer);
-            $result = json_decode($uitpas, true);
+        $uitpas = (new UitpasController)->uitpasKind($kind->rijksregisternummer);
+        $result = json_decode($uitpas, true);
 
-            // als uitpas bestaat, vul kolommen aan
-            if(array_key_exists('uitpasNumber', $result))
-            {
-                $kind->update([
-                    'uitpasKansentarief' => $result['socialTariff']['status'] ,
-                    'uitpasDatumCheck' => today(),
-                    'uitpasnummer' => $result['uitpasNumber']
-                ]);
-                if(array_key_exists('messages', $result))
-                {
-                    $kind->update(['uitpasTekst' => $result['messages'][0]['text'],]);
-                }
-                else
-                {
-                    $kind->update(['uitpasTekst' => '']);
-                }
+        // als uitpas bestaat, vul kolommen aan
+        if (array_key_exists('uitpasNumber', $result)) {
+            $kind->update([
+                'uitpasKansentarief' => $result['socialTariff']['status'],
+                'uitpasDatumCheck' => today(),
+                'uitpasnummer' => $result['uitpasNumber']
+            ]);
+            if (array_key_exists('messages', $result)) {
+                $kind->update(['uitpasTekst' => $result['messages'][0]['text'],]);
+            } else {
+                $kind->update(['uitpasTekst' => '']);
             }
-            // als uitpas niet bestaat, wis kolommen
-            else
-            {
-                $kind->update([
-                    'uitpasnummer' => '',
-                    'uitpasKansentarief' => '',
-                    'uitpasTekst' => '',
-                    'uitpasDatumCheck' => today()
-                ]);
-            }
+        } // als uitpas niet bestaat, wis kolommen
+        else {
+            $kind->update([
+                'uitpasnummer' => '',
+                'uitpasKansentarief' => '',
+                'uitpasTekst' => '',
+                'uitpasDatumCheck' => today()
+            ]);
+        }
 
     }
 
     public function editAdminAnimatorInfo(Request $request, $id)
     {
         $validated = $request->validate([
-            'infoAdminAnimator'=> 'nullable|string|max:510',
+            'infoAdminAnimator' => 'nullable|string|max:510',
             'kind' => 'string|max:20'
         ]);
         $kind = Kind::find($validated['kind']);
