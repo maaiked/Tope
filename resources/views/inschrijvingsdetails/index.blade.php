ecfc72e0853d99fb19fce3c819ffc04dd9d78790
yonicelen <yoni.celen@student.ehb.be>
2024-06-10 14:08:09 +0200

    added search to inschrijvingen and fixed title for admin

--- resources/views/inschrijvingsdetails/index.blade.php
+++ resources/views/inschrijvingsdetails/index.blade.php
@@ -1,77 +1,58 @@
 <x-app-layout>
-
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            @if (auth()->user()->isAdmin)
+            {{ __('Alle inschrijvingen') }}
+            @else
             {{ __('Jouw inschrijvingen') }}
+            @endif
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-
             @if (session('status') === 'inschrijving-verwijderd')
-                <p class="text-lg py-4 px-4 text-md font-bold underline text-red-600"
-                ><i>{{ __('Inschrijving werd geannuleerd.') }}</i></p>
+            <p class="text-lg py-4 px-4 text-md font-bold underline text-red-600">
+                <i>{{ __('Inschrijving werd geannuleerd.') }}</i>
+            </p>
             @endif
 
-{{--                TODO:: fix small screen colums--}}
-               <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg px-6 py-6 grid gap-4 gap-y-2 text-sm grid-cols-2 md:grid-cols-5">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg px-6 py-6">
 
-                {{-- kolomtitels --}}
-                <div class="flex space-x-0">
-                    <p class="mt-4 text-lg text-gray-900">Inschrijvingsdatum</p>
-                </div>
-                <div class="flex space-x-0">
-                    <p class="mt-4 text-lg text-gray-900">Deelnemer</p>
-                </div>
-                <div class="flex space-x-0">
-                    <p class="mt-4 text-lg text-gray-900">Activiteit</p>
-                </div>
-                <div class="flex space-x-0">
-                    <p class="mt-4 text-lg text-gray-900">Totaalprijs</p>
-                </div>
-                <div class="flex space-x-0">
-                    <p class="mt-4 text-lg text-gray-900">Knoppen</p>
-                </div>
-
-                {{--                TODO: inschrijvingen sorteren op datum inschrijving of activiteit? groeperen per jaar > vakantieperiode? filters toevoegen?--}}
-
-
-                {{-- rijen --}}
-                @foreach ($inschrijvingsdetails as $inschrijvingsdetail)
-
-                    {{--    inschrijvingsdatum    --}}
-                    <div class="flex space-x-0">
-                        <p class="mt-2 text-md text-gray-900">{{ Carbon\Carbon::parse($inschrijvingsdetail->inschrijvingsdatum)->format('d-m-Y')}}</p>
-                    </div>
-
-                    {{--    voornaam + familienaam kind    --}}
-                    <div class="flex space-x-0">
-                        <p class="mt-2 text-md text-gray-900">{{ $inschrijvingsdetail->kind->voornaam }}</p>
-
-                        <p class="mt-2 text-md px-2 text-gray-900">{{ $inschrijvingsdetail->kind->familienaam }}</p>
-                    </div>
-
-                    {{--    naam activiteit    --}}
-                    <div class=" flex space-x-0">
-                        <p class="mt-2 text-md text-gray-900">{{ $inschrijvingsdetail->activiteit->naam." : ".Carbon\Carbon::parse($inschrijvingsdetail->activiteit->starttijd)->format('d-m-Y')." - ".Carbon\Carbon::parse($inschrijvingsdetail->activiteit->eindtijd)->format('d-m-Y')}}</p>
-                    </div>
-
-                    {{--    prijs activiteit    --}}
-                    <div class=" flex space-x-0">
-                        <p class="mt-2 text-md text-gray-900">{{ "€ ".$inschrijvingsdetail->prijs }}</p>
-                    </div>
-
-                    {{--     knop details + uitschrijven     --}}
-                    <div class="flex space-x-0">
-                        <button class="rounded-md bg-gray-500 text-white focus:ring-gray-600 px-4 py-2 text-sm m-4"
-                                onclick="window.location='{{ route("inschrijvingsdetails.show", $inschrijvingsdetail->inschrijvingsdetails_id) }}'">
-                                {{ "details" }}
-                        </button>
-                    @if($inschrijvingsdetail->activiteit->annulerenTot > today())
-                            <button class="rounded-md bg-red-900 text-white focus:ring-gray-600 px-4 py-2 text-sm m-4"
+                <table id="inschrijvingen" class="table-auto w-full">
+                    <thead>
+                    <tr>
+                        <th class="px-4 py-2">Inschrijvingsdatum</th>
+                        <th class="px-4 py-2">Deelnemer</th>
+                        <th class="px-4 py-2">Activiteit</th>
+                        <th class="px-4 py-2">Totaalprijs</th>
+                        <th class="px-4 py-2">Knoppen</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($inschrijvingsdetails as $inschrijvingsdetail)
+                    <tr>
+                        <td class="border px-4 py-2">
+                            {{ Carbon\Carbon::parse($inschrijvingsdetail->inschrijvingsdatum)->format('d-m-Y') }}
+                        </td>
+                        <td class="border px-4 py-2">
+                            {{ $inschrijvingsdetail->kind->voornaam }} {{ $inschrijvingsdetail->kind->familienaam }}
+                        </td>
+                        <td class="border px-4 py-2">
+                            {{ $inschrijvingsdetail->activiteit->naam." : ".Carbon\Carbon::parse($inschrijvingsdetail->activiteit->starttijd)->format('d-m-Y')." - ".Carbon\Carbon::parse($inschrijvingsdetail->activiteit->eindtijd)->format('d-m-Y') }}
+                        </td>
+                        <td class="border px-4 py-2">
+                            {{ "€ ".$inschrijvingsdetail->prijs }}
+                        </td>
+                        <td class="border px-4 py-2 flex space-x-2">
+                            <button class="rounded-md bg-gray-500 text-white focus:ring-gray-600 px-4 py-2 text-sm"
+                                    onclick="window.location='{{ route("inschrijvingsdetails.show", $inschrijvingsdetail->inschrijvingsdetails_id) }}'">
+                            {{ "details" }}
+                            </button>
+                            @if($inschrijvingsdetail->activiteit->annulerenTot > today())
+                            <button class="rounded-md bg-red-900 text-white focus:ring-gray-600 px-4 py-2 text-sm"
                                     onclick="show({{$inschrijvingsdetail->inschrijvingsdetails_id}})">
-                                    {{ "uitschrijven" }}
+                                {{ "uitschrijven" }}
                             </button>
                             <!-- Modal -->
                             <div id="confirm.{{$inschrijvingsdetail->inschrijvingsdetails_id}}" class="fixed z-10 inset-0 overflow-y-auto hidden">
@@ -92,43 +73,53 @@
                                         </div>
                                         <div class="content-center">
                                             <p class="px-2 py-2">Wil je volgende inschrijving annuleren? </p>
-                                            <p class="px-2 py-2 font-bold">{{ $inschrijvingsdetail->activiteit->naam." : ".$inschrijvingsdetail->kind->voornaam." ".$inschrijvingsdetail->kind->familenaam }}</p>
+                                            <p class="px-2 py-2 font-bold">{{ $inschrijvingsdetail->activiteit->naam." : ".$inschrijvingsdetail->kind->voornaam." ".$inschrijvingsdetail->kind->familienaam }}</p>
                                             <button
                                                 class="rounded-md bg-blue-600 text-white focus:ring-blue-400 px-2 py-2 text-sm"
                                                 onclick="window.location='{{ route("inschrijvingsdetails.destroy", $inschrijvingsdetail->inschrijvingsdetails_id) }}'">
-                                                {{ "Inschrijving annuleren" }}
+                                            {{ "Inschrijving annuleren" }}
                                             </button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                             @else
-                            <p class="m-4">uitschrijven niet meer mogelijk</p>
-                        @endif
-                    </div>
-                @endforeach
+                            @else
+                            <p>uitschrijven niet meer mogelijk</p>
+                            @endif
+                        </td>
+                    </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+
+                {{-- Scripts voor tabel met search, sorting en paginatie --}}
+                <link rel="stylesheet" href="https://cdn.datatables.net/2.0.3/css/dataTables.dataTables.css">
+                <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+                <script src="https://cdn.datatables.net/2.0.3/js/dataTables.js"></script>
+
+                <script>
+                    $(document).ready(function() {
+                        $('#inschrijvingen').DataTable({
+                            scrollX: true,
+                        });
+                    });
+
+                    function show(id) {
+                        var name = "confirm.".concat(id);
+                        var modal = document.getElementById(name);
+                        modal.classList.remove('hidden');
+                    }
+
+                    function hide(id) {
+                        var name = "confirm.".concat(id);
+                        var modal = document.getElementById(name);
+                        modal.classList.add('hidden');
+                    }
+                </script>
             </div>
 
-            {{--    paginatie    --}}
-             {{ $inschrijvingsdetails->links() }}
+            {{-- Paginatie --}}
+            {{ $inschrijvingsdetails->links() }}
         </div>
     </div>
-
-    <script>
-
-        // JavaScript to toggle the modal
-
-        function show(id) {
-            var name = "confirm.".concat(id);
-            var modal = document.getElementById(name);
-            modal.classList.remove('hidden');
-        }
-
-        function hide(id) {
-            var name = "confirm.".concat(id);
-            var modal = document.getElementById(name);
-            modal.classList.add('hidden');
-        }
-
-    </script>
 </x-app-layout>
