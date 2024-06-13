7915736095e4ba803dd6bc28928ed29c60bf6be2
Maaiked <maaike.dupont@student.ehb.be>
2024-06-13 17:33:27 +0200

    updated responsive menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800"/>
                     </a>
                 </div>
 
@@ -19,30 +19,31 @@
                         {{ __('Activiteiten') }}
                     </x-nav-link>
                     @if(!Auth::user()->isAnimator)
-                    <x-nav-link :href="route('inschrijvingsdetails.index')" :active="request()->routeIs('inschrijvingsdetails.index')">
-                        {{ __('Inschrijvingen') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('kinderen.index')" :active="request()->routeIs('kinderen.index')">
-                        {{ __('Kinderen') }}
-                    </x-nav-link>
+                        <x-nav-link :href="route('inschrijvingsdetails.index')"
+                                    :active="request()->routeIs('inschrijvingsdetails.index')">
+                            {{ __('Inschrijvingen') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('kinderen.index')" :active="request()->routeIs('kinderen.index')">
+                            {{ __('Kinderen') }}
+                        </x-nav-link>
                     @endif
                     @if(Auth::user()->isAdmin)
-                    <x-nav-link :href="route('profiel.index')" :active="request()->routeIs('profiel.index')">
-                        {{ __('Ouders') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('profile.index')" :active="request()->routeIs('profile.index')">
-                        {{ __('Gebruikers') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('locatie.index')" :active="request()->routeIs('locatie.index')">
-                        {{ __('Locaties') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('uitpas.index')" :active="request()->routeIs('uitpas.index')">
-                        {{ __('UiTPAS') }}
-                    </x-nav-link>
+                        <x-nav-link :href="route('profiel.index')" :active="request()->routeIs('profiel.index')">
+                            {{ __('Ouders') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('profile.index')" :active="request()->routeIs('profile.index')">
+                            {{ __('Gebruikers') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('locatie.index')" :active="request()->routeIs('locatie.index')">
+                            {{ __('Locaties') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('uitpas.index')" :active="request()->routeIs('uitpas.index')">
+                            {{ __('UiTPAS') }}
+                        </x-nav-link>
                     @endif
                     @if(!Auth::user()->isAdmin && !Auth::user()->isAnimator)
                         <x-nav-link :href="route('profiel.create')" :active="request()->routeIs('profiel.create')">
-                        {{ __('Profiel') }}
+                            {{ __('Profiel') }}
                         </x-nav-link>
                     @endif
                 </div>
@@ -52,11 +53,15 @@
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                           {{ Auth::user()->profiel->voornaam ?? 'Uitloggen' }}
+                        <button
+                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                            {{ Auth::user()->profiel->voornaam ?? 'Uitloggen' }}
                             <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                     viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd"
+                                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                          clip-rule="evenodd"/>
                                 </svg>
                             </div>
                         </button>
@@ -72,7 +77,7 @@
                             @csrf
 
                             <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                                             onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Afmelden') }}
                             </x-dropdown-link>
@@ -83,10 +88,14 @@
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                <button @click="open = ! open"
+                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
+                              stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M4 6h16M4 12h16M4 18h16"/>
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
+                              stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
                     </svg>
                 </button>
             </div>
@@ -99,23 +108,34 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('activiteiten.index')" :active="request()->routeIs('activiteiten.index')">
+            <x-responsive-nav-link :href="route('activiteiten.index')"
+                                   :active="request()->routeIs('activiteiten.index')">
                 {{ __('Activiteiten') }}
-             </x-responsive-nav-link>
+            </x-responsive-nav-link>
             @if(!Auth::user()->isAnimator)
-                <x-responsive-nav-link :href="route('inschrijvingsdetails.index')" :active="request()->routeIs('inschrijvingsdetails.index')">
+                <x-responsive-nav-link :href="route('inschrijvingsdetails.index')"
+                                       :active="request()->routeIs('inschrijvingsdetails.index')">
                     {{ __('Inschrijvingen') }}
                 </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('kinderen.index')" :active="request()->routeIs('kinderen.index')">
-                {{ __('Kinderen') }}
-            </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('kinderen.index')" :active="request()->routeIs('kinderen.index')">
+                    {{ __('Kinderen') }}
+                </x-responsive-nav-link>
             @endif
             @if(!Auth::user()->isAdmin && !Auth::user()->isAnimator)
-            <x-responsive-nav-link :href="route('profiel.create')" :active="request()->routeIs('profiel.create')">
-                {{ __('Profiel') }}
-            </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('profiel.create')" :active="request()->routeIs('profiel.create')">
+                    {{ __('Profiel') }}
+                </x-responsive-nav-link>
             @endif
             @if(Auth::user()->isAdmin)
+                <x-responsive-nav-link :href="route('profiel.index')" :active="request()->routeIs('profiel.index')">
+                    {{ __('Ouders') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('profile.index')" :active="request()->routeIs('profile.index')">
+                    {{ __('Gebruikers') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('locatie.index')" :active="request()->routeIs('locatie.index')">
+                    {{ __('Locaties') }}
+                </x-responsive-nav-link>
                 <x-responsive-nav-link :href="route('uitpas.index')" :active="request()->routeIs('uitpas.index')">
                     {{ __('UiTPAS') }}
                 </x-responsive-nav-link>
@@ -125,7 +145,8 @@
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->profiel->voornaam ?? 'Afmelden' }}</div>
+                <div
+                    class="font-medium text-base text-gray-800">{{ Auth::user()->profiel->voornaam ?? 'Afmelden' }}</div>
             </div>
 
             <div class="mt-3 space-y-1">
@@ -138,7 +159,7 @@
                     @csrf
 
                     <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                                           onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Afmelden') }}
                     </x-responsive-nav-link>
